3e14dda61ae9928a187bee4966a1e5d9813f84f2
vinaykannambadi <vinaykannambadi@gmail.com>
2021-01-22 13:17:11 +0530

    ngrx-select

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import {select, Store} from "@ngrx/store";
 import {Observable} from "rxjs";
 import {map} from 'rxjs/operators';
 import {NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router} from '@angular/router';
+import { AppState } from './reducers';
+import { isLoggedIn, isLoggedOut } from './auth/selectors/auth.selector';
 
 @Component({
   selector: 'app-root',
@@ -12,8 +14,10 @@ import {NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Route
 export class AppComponent implements OnInit {
 
     loading = true;
+    isLoggedIn$: Observable<boolean>;
+    isLoggedOut$: Observable<boolean>;
 
-    constructor(private router: Router) {
+    constructor(private router: Router, private store: Store<AppState>) {
 
     }
 
@@ -37,6 +41,14 @@ export class AppComponent implements OnInit {
           }
         }
       });
+
+      this.isLoggedIn$ = this.store.pipe(
+        select(isLoggedIn)
+      );
+
+      this.isLoggedOut$ = this.store.pipe(
+        select(isLoggedOut)
+      );
 
     }
 
